99a011b600eaba95f30e64f24f2321a204cb4570
Grisha <grisha765@tuta.io>
2024-10-27 17:09:58 +0300

    [+] add configs

---
+++ config/config.py
@@ -0,0 +1,26 @@
+import os
+
+class Config:
+    log_level: str = "INFO"
+    tg_id: str = '1'
+    tg_hash: str = 'b6b154c3707471f5339bd661645ed3d6'
+    tg_token: str = 'None'
+    model_size: str = 'tiny' #tiny, tiny.en, base, base.en, small, small.en, medium, medium.en, large-v1, large-v2, large-v3, or large
+    cpu_threads: str = '4'
+    
+    @classmethod
+    def load_from_env(cls):
+        for key, value in cls.__annotations__.items():
+            env_value = os.getenv(key.upper())
+            if env_value is not None:
+                if isinstance(value, int):
+                    setattr(cls, key, int(env_value))
+                elif isinstance(value, list):
+                    setattr(cls, key, env_value.split(","))
+                else:
+                    setattr(cls, key, env_value)
+
+Config.load_from_env()
+
+if __name__ == "__main__":
+    raise RuntimeError("This module should be run only via main.py")
