bf697a61b867e61982f6f97dd10218fac0cdf651
Grisha <grisha765@tuta.io>
2025-05-31 04:10:51 +0300

    [r] eng comments

--- config/logging_config.py
+++ config/logging_config.py
@@ -2,32 +2,30 @@ import logging
 import os
 
 from config.config import Config 
-# Определяем ANSI escape-коды для цветов
 RESET = "\x1b[0m"
 WHITE = "\x1b[0m"
 COLORS = {
-    'DEBUG': "\x1b[34m",      # Синий
-    'INFO': "\x1b[32m",       # Зеленый
-    'WARNING': "\x1b[33m",    # Желтый
-    'ERROR': "\x1b[31m",      # Красный
-    'CRITICAL': "\x1b[41m",   # Красный фон
+    'DEBUG': "\x1b[34m",      # Blue
+    'INFO': "\x1b[32m",       # Green
+    'WARNING': "\x1b[33m",    # Yellow
+    'ERROR': "\x1b[31m",      # Red
+    'CRITICAL': "\x1b[41m",   # Red background
 }
 
 class ColoredFormatter(logging.Formatter):
     def format(self, record):
         log_color = COLORS.get(record.levelname, RESET)
         message = super().format(record)
-        # Извлечение имени файла без подчеркиваний
         filename = os.path.splitext(os.path.basename(record.pathname))[0]
         return f"{log_color}{record.levelname}{RESET}{WHITE}: {filename}: {message}{RESET}"
 
 def setup_logging(name='my_app'):
     logger = logging.getLogger(name)
-    if not logger.handlers:  # Проверяем, есть ли уже обработчики у логгера
+    if not logger.handlers:
         handler = logging.StreamHandler()
         formatter = ColoredFormatter('%(message)s')
         handler.setFormatter(formatter)
         logger.addHandler(handler)
-        logger.setLevel(getattr(logging, Config.log_level, logging.INFO))  # Устанавливаем уровень логирования явно
+        logger.setLevel(getattr(logging, Config.log_level, logging.INFO))
     return logger
 
